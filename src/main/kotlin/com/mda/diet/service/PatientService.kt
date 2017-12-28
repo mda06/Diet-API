24f405b628d9ee37a021324ccb98566b553476d7
Michaël Van Meerbeek <michael.van.meerbeek@student.ehb.be>
2017-12-28 15:16:16 +0100

    Add patientId to param when add a new patient

--- src/main/kotlin/com/mda/diet/service/PatientService.kt
+++ src/main/kotlin/com/mda/diet/service/PatientService.kt
@@ -1,7 +1,8 @@
 package com.mda.diet.service
 
-import com.mda.diet.model.Dietetist
+import com.mda.diet.model.AnthropometricParameter
 import com.mda.diet.model.Patient
+import com.mda.diet.repository.AnthropometricParameterRepository
 import com.mda.diet.repository.CustomerRepository
 import com.mda.diet.repository.PatientRepository
 import org.springframework.security.core.context.SecurityContextHolder
@@ -9,7 +10,8 @@ import org.springframework.stereotype.Service
 
 @Service
 class PatientService(val repository: PatientRepository,
-                     val customerRepository: CustomerRepository) {
+                     val customerRepository: CustomerRepository,
+                     val paramRepository: AnthropometricParameterRepository) {
     fun findAll() = repository.findAll()
 
     fun getById(id: Long)
@@ -24,7 +26,23 @@ class PatientService(val repository: PatientRepository,
             //Test if customer is a dietetist
             patient.dietetistId = customer.id
         }
-        return repository.save(patient)
+        if(patient.id == 0L) {
+            //The param needs the id of the patient
+            //But the id is only generated here so we first save the patient and after that the params
+            val params = patient.anthropometricParameters
+            val paramsReturn = arrayListOf<AnthropometricParameter>()
+            patient.anthropometricParameters = arrayListOf()
+            val patientReturn = repository.save(patient)
+            for (param in params) {
+                param.patientId = patientReturn.id
+                paramsReturn.add(paramRepository.save(param))
+            }
+            //After that we put the array back
+            patientReturn.anthropometricParameters = paramsReturn
+            return patientReturn
+        } else {
+            return repository.save(patient);
+        }
     }
 
     fun updatePatient(id: Long, patient: Patient) : Patient {
