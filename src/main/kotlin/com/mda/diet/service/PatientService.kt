e457f90fde43954985ec47e03ee982e37e0c6a6e
Michaël Van Meerbeek <michael.van.meerbeek@student.ehb.be>
2018-01-04 13:32:06 +0100

    Return an error if no patient was found

--- src/main/kotlin/com/mda/diet/service/PatientService.kt
+++ src/main/kotlin/com/mda/diet/service/PatientService.kt
@@ -16,7 +16,7 @@ class PatientService(val repository: PatientRepository,
     fun findAll() = repository.findAll()
 
     fun getById(id: Long)
-            = repository.findOne(id)// ?: throw CustomNotFoundException("Not found patient with id $id")
+            = repository.findOne(id) ?: throw CustomNotFoundException("Not found patient with id $id")
 
     fun getByDiet(id: Long)
             =  repository.getAllByDietetistId(id)
