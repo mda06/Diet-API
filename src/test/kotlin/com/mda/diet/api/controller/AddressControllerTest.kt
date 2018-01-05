c11dc60ff79520e96bb2e09743ba7d22ba3692dd
Michaël Van Meerbeek <michael.van.meerbeek@student.ehb.be>
2018-01-05 19:03:55 +0100

    First test to deploy

--- src/test/kotlin/com/mda/diet/api/controller/AddressControllerTest.kt
+++ src/test/kotlin/com/mda/diet/api/controller/AddressControllerTest.kt
@@ -1,6 +1,5 @@
 package com.mda.diet.api.controller
 
-import com.fasterxml.jackson.databind.ObjectMapper
 import com.mda.diet.error.ApiError
 import com.mda.diet.error.CustomNotFoundException
 import com.mda.diet.model.Address
@@ -8,8 +7,6 @@ import com.mda.diet.service.AddressService
 import org.junit.BeforeClass
 import org.junit.Test
 import org.junit.runner.RunWith
-import org.mockito.Matchers.any
-import org.mockito.Matchers.anyObject
 import org.mockito.Mockito
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.context.SpringBootTest
@@ -33,7 +30,7 @@ class AddressControllerTest {
         @BeforeClass
         fun init() {
             //TODO change this with a request to /auth/login ? Store in a file ?
-            headers.add("Authorization", "Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJSUzI1NiIsImtpZCI6Ik56ZzRNVUZGUkRRelFUTTBNVVUxUlRJME1USkVPVEUyUlRNMlJVVkNNREJFTWpRNE9Ua3lSQSJ9.eyJpc3MiOiJodHRwczovL21kYXRlc3QuZXUuYXV0aDAuY29tLyIsInN1YiI6ImF1dGgwfDVhMzhkZDgzOGQzY2VhN2FhZDQwZTVhZCIsImF1ZCI6WyJodHRwczovL2RpZXQtbWFuYWdlbWVudC5jb20iLCJodHRwczovL21kYXRlc3QuZXUuYXV0aDAuY29tL3VzZXJpbmZvIl0sImlhdCI6MTUxNTE0NzAzNiwiZXhwIjoxNTE1MjMzNDM2LCJhenAiOiI1bm5SWFVpV3F4OWNNUzY1b0FOV01hUDN4ZUUxNlZBMSIsInNjb3BlIjoib3BlbmlkIHByb2ZpbGUgZW1haWwgYWRkcmVzcyBwaG9uZSIsImd0eSI6InBhc3N3b3JkIn0.NS5a4TCxu9z8j2QT0_Bsz1YpOTwOkPLvkomCM5ELblALNxb5X_7dzliCp0arCqLUEbj8yVvMCx_EU-npz7-LX9Vca13oenvHEGuD6jLMN2RiireCxWx1ZZPKaklZ0hzybtdP_TlBxgLI2YZU8_POOkDCbHnZeRzXF-t8EgO5mmmKL42BXdb6HyVs5Dbxas-9nRBBTf0s2qEYTfg1FGzdjXRahVRKB3ZbPK0XRchrlaRI7WpOJ7kIBXatgl3JpjDDZG9DSziVSXxNKlAOUOmawgdb9hCRrV-AvKY0x6dHOSaTczTGmwbsIyQvx6KUnWSbkhFhNpUDZ3S9z8CjnoOi1A")
+            headers.add("Authorization", "Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJSUzI1NiIsImtpZCI6Ik56ZzRNVUZGUkRRelFUTTBNVVUxUlRJME1USkVPVEUyUlRNMlJVVkNNREJFTWpRNE9Ua3lSQSJ9.eyJpc3MiOiJodHRwczovL21kYXRlc3QuZXUuYXV0aDAuY29tLyIsInN1YiI6ImF1dGgwfDVhMzhkZDgzOGQzY2VhN2FhZDQwZTVhZCIsImF1ZCI6WyJodHRwczovL2RpZXQtbWFuYWdlbWVudC5jb20iLCJodHRwczovL21kYXRlc3QuZXUuYXV0aDAuY29tL3VzZXJpbmZvIl0sImlhdCI6MTUxNTE1NTI4OSwiZXhwIjoxNTE1MjQxNjg5LCJhenAiOiI1bm5SWFVpV3F4OWNNUzY1b0FOV01hUDN4ZUUxNlZBMSIsInNjb3BlIjoib3BlbmlkIHByb2ZpbGUgZW1haWwgYWRkcmVzcyBwaG9uZSBzY29wZTpkaWV0IiwiZ3R5IjoicGFzc3dvcmQifQ.M9lusJLVWxsMGpQ_KvOt5QWBYiqX56PvE8PhcgSiFKWmTx5qn2tjuOQqZUxbMR2xlydgmc-aDj8cLTzuMqZOZRCU3cNyMVs9bUwjUG6-NxujtiaQnv5YfJSkifKaqJ5l6F_qOpO02Ysbgm74CD1jbBJWwFp0999WekJaYn8z7gC0waq-my36Fz_NBcgzApvloiQNLm52rKWhea6AfZqKeKnQEs93Jobdv-rAjjcvW_bYGeFBLMsVKf89sWmG7FkcBymZVgl5yuyCoQSu2rPOsOO1MMFa2iOmZH4-T9kMoB-eUpgGeeVW53O-8EadezBBSxdsirvOB_GuVj0rOIBplg")
             headers.add("Content-Type", "application/json")
         }
 
