ebb7c5af8db3abb3a49dc76fedcece997366d3cd
Michaël Van Meerbeek <michael.van.meerbeek@student.ehb.be>
2018-01-11 17:12:39 +0100

    Some unit test for product

--- src/test/kotlin/com/mda/diet/api/service/ProductServiceTest.kt
+++ src/test/kotlin/com/mda/diet/api/service/ProductServiceTest.kt
@@ -1,5 +1,6 @@
 package com.mda.diet.api.service
 
+import com.mda.diet.error.CustomNotFoundException
 import com.mda.diet.model.Product
 import com.mda.diet.model.ProductTranslation
 import com.mda.diet.repository.ProductRepository
@@ -9,7 +10,6 @@ import org.junit.runner.RunWith
 import org.mockito.InjectMocks
 import org.mockito.Mock
 import org.mockito.Mockito
-import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.test.context.junit4.SpringRunner
 import kotlin.test.*
 
@@ -24,12 +24,33 @@ class ProductServiceTest {
     var service: ProductService? = null
 
     @Test
-    fun testGetProductById() {
+    fun testSize() {
+        Mockito.`when`(repository!!.count()).thenReturn(0)
+        assertEquals(0, service!!.getSize())
+        Mockito.`when`(repository!!.count()).thenReturn(15)
+        assertEquals(15, service!!.getSize())
+    }
+
+    @Test
+    fun testGetProductByIdSuccessLanguage() {
         Mockito.`when`(repository!!.findByIdAndTranslationsLanguageEquals(5633, "en"))
                 .thenReturn(Product(5633)
-                        .also { it.translations.add(ProductTranslation(1, "en", "Apricot")) }
+                        .also { it.translations.add(ProductTranslation(1, "en", "Apricot"))
+                            it.translations.add(ProductTranslation(2, "fr", "Abricot"))}
                 )
         val prod = service!!.getProductById(5633, "en")
         assertEquals("Apricot", prod.name)
+    }
+
+    @Test
+    fun testGetProductByIdFailId() {
+        Mockito.`when`(repository!!.findByIdAndTranslationsLanguageEquals(5632, "en"))
+                .thenReturn(null)
+        try {
+            service!!.getProductById(5632, "en")
+            fail("Whould throw a exception")
+        } catch(ex: CustomNotFoundException) {
+            assertEquals("Not found product with id 5632", ex.message)
+        }
     }
 }
