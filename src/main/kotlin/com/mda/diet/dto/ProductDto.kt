10d6e04eedb338561c4e4946dee821aa011b7c11
Michaël Van Meerbeek <michael.van.meerbeek@student.ehb.be>
2018-01-25 18:31:56 +0100

    BugFix when uploading new products the fav parameter wasn't good set

--- src/main/kotlin/com/mda/diet/dto/ProductDto.kt
+++ src/main/kotlin/com/mda/diet/dto/ProductDto.kt
@@ -7,17 +7,17 @@ import com.mda.diet.model.ProductTranslation
 data class ProductDto ( var id: Long = 0,
                         val imgUrl: String = "",
                         val name: String = "",
-                        val favorite: Boolean = false,
                         val units: List<Nutriment> = arrayListOf(),
                         val macros: List<Nutriment> = arrayListOf(),
-                        val micros: List<Nutriment> = arrayListOf()) {
+                        val micros: List<Nutriment> = arrayListOf(),
+                        val favorite: Boolean = false) {
 
     constructor(prod: Product, favorite: Boolean) : this(prod.id, prod.imgUrl,
             if(prod.translations.size > 0) prod.translations[0].name else "",
-            favorite,
             if(prod.translations.size > 0) prod.translations[0].units else arrayListOf(),
             if(prod.translations.size > 0) prod.translations[0].macros else arrayListOf(),
-            if(prod.translations.size > 0) prod.translations[0].micros else arrayListOf())
+            if(prod.translations.size > 0) prod.translations[0].micros else arrayListOf(),
+            favorite)
 
     fun toProduct(language: String): Product {
         val prod = Product(id, imgUrl, arrayListOf(ProductTranslation(0, language, name, id,
