463fe01fe7aace189fce2ed3c5be425186382d3c
Michaël Van Meerbeek <michael.van.meerbeek@student.ehb.be>
2018-01-27 16:14:02 +0100

    Refactor test

--- src/test/kotlin/com/mda/diet/api/service/MenuServiceTest.kt
+++ src/test/kotlin/com/mda/diet/api/service/MenuServiceTest.kt
@@ -1,18 +1,17 @@
 package com.mda.diet.api.service
 
 import com.mda.diet.dto.MealDto
+import com.mda.diet.dto.MealProductDto
 import com.mda.diet.dto.MenuDto
 import com.mda.diet.error.CustomNotFoundException
-import com.mda.diet.model.Meal
-import com.mda.diet.model.Menu
-import com.mda.diet.model.Patient
-import com.mda.diet.model.Product
+import com.mda.diet.model.*
 import com.mda.diet.repository.MenuRepository
 import com.mda.diet.repository.PatientRepository
 import com.mda.diet.repository.ProductRepository
 import com.mda.diet.service.MenuService
 import org.junit.Test
 import org.junit.runner.RunWith
+import org.mockito.AdditionalAnswers
 import org.mockito.InjectMocks
 import org.mockito.Mock
 import org.mockito.Mockito
@@ -52,21 +51,27 @@ class MenuServiceTest {
     //Success
     @Test
     fun testAddMenuSuccess() {
-        /*val dto = MenuDto(0, 1, LocalDate.of(2018, 1, 25), listOf(
+        val dto = MenuDto(0, 1, LocalDate.of(2018, 1, 25), listOf(
                 MealDto(0, "Breakfast", "Don't use to much oil", 0, "",
-                        listOf(1, 2)),
+                        listOf(MealProductDto(150, 1), MealProductDto(200, 2))),
                 MealDto(0, "Diner", "Chicken not burned", 0, "",
-                        listOf(3, 4))
+                        listOf(MealProductDto(50, 3), MealProductDto(300, 4)))
         ))
-        Mockito.`when`(patientRepo!!.findOne(1)).thenReturn(Patient(1))
-        Mockito.`when`(productRepo!!.findAll(listOf(1L, 2L))).thenReturn(listOf(Product(1), Product(2)))
-        Mockito.`when`(productRepo!!.findAll(listOf(3L, 4L))).thenReturn(listOf(Product(3)))
+        Mockito.`when`(patientRepo!!.findOne(Mockito.any())).thenAnswer {
+            Patient(it.getArgumentAt(0, Long::class.java))
+        }
+
+        Mockito.`when`(productRepo!!.findOne(Mockito.any())).thenAnswer {
+            Product(it.getArgumentAt(0, Long::class.java))
+        }
         Mockito.`when`(repository!!.save(Mockito.any<Menu>()))
                 .thenReturn(Menu(1, dto.date, arrayListOf(
                         Meal(1, "Breakfast", "Don't use to much oil", 0, "",null,
-                                arrayListOf(Product(1), Product(2))),
+                                arrayListOf(MealProduct(null, Product(1), 150),
+                                        MealProduct(null, Product(2), 200))),
                         Meal(2, "Diner", "Chicken not burned", 0, "",null,
-                                arrayListOf(Product(3), Product(4)))
+                                arrayListOf(MealProduct(null, Product(3), 50),
+                                        MealProduct(null, Product(4), 300)))
                 ), Patient(dto.patientId)))
         val menu = service!!.addMenu(dto)
         assertEquals(1, menu.id)
@@ -77,9 +82,14 @@ class MenuServiceTest {
             assertEquals(dto.meals[i].name, menu.meals[i].name)
             assertEquals(dto.meals[i].extraInfo, menu.meals[i].extraInfo)
             assertEquals(dto.meals[i].score, menu.meals[i].score)
-            assertEquals(dto.meals[i].productIds[i], menu.meals[i].productIds[i])
-            assertEquals(dto.meals[i].productIds[1], menu.meals[i].productIds[1])
-        }*/
+
+
+            for(j in menu.meals.indices) {
+                assertEquals(dto.meals[i].mealProducts[j].productId, menu.meals[i].mealProducts[j].productId)
+                assertEquals(dto.meals[i].mealProducts[j].quantity, menu.meals[i].mealProducts[j].quantity)
+            }
+
+        }
     }
 
     @Test
