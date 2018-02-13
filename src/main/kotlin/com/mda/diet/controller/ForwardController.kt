20d3624728d03a43a8a195b7baf6715731183f54
MDA06 <michaelvm171@msn.com>
2018-02-13 16:46:42 +0100

    Enable routing urls for Angular (only /api is authenticated)

---
+++ src/main/kotlin/com/mda/diet/controller/ForwardController.kt
@@ -0,0 +1,14 @@
+package com.mda.diet.controller
+
+import org.springframework.stereotype.Controller
+import org.springframework.web.bind.annotation.RequestMapping
+
+
+@Controller
+class ForwardController {
+
+    @RequestMapping(value = ["/**/{[path:[^\\.]*}"])
+    fun redirect(): String {
+        return "forward:/"
+    }
+}
