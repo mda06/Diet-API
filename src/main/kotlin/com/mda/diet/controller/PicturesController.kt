3c4d23084918cb20725d7c4904f4e3c3185c751e
MDA06 <michaelvm171@msn.com>
2018-04-11 12:04:51 +0200

    Added a date as parameter for the pictures

--- src/main/kotlin/com/mda/diet/controller/PicturesController.kt
+++ src/main/kotlin/com/mda/diet/controller/PicturesController.kt
@@ -1,15 +1,20 @@
 package com.mda.diet.controller
 
 import com.mda.diet.service.PictureService
+import org.springframework.format.annotation.DateTimeFormat
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
 import org.springframework.web.multipart.MultipartFile
 import org.springframework.web.bind.annotation.PostMapping
+import org.springframework.web.bind.annotation.RequestParam
+import java.time.LocalDate
 
 @RestController
 @RequestMapping("$prefix/pictures")
 class PicturesController(val service: PictureService) {
     @PostMapping("/upload")
-    fun handleFileUpload(pictures: List<MultipartFile>)
-        = service.handleFileUpload(pictures)
+    fun handleFileUpload(pictures: List<MultipartFile>,
+                            @RequestParam(name = "date", required = false)
+                            @DateTimeFormat(iso = DateTimeFormat.ISO.DATE)date: LocalDate?)
+        = service.handleFileUpload(pictures, date)
 }
--- src/main/kotlin/com/mda/diet/service/PictureService.kt
+++ src/main/kotlin/com/mda/diet/service/PictureService.kt
@@ -7,9 +7,11 @@ import java.time.LocalDate
 
 @Service
 class PictureService {
-    fun handleFileUpload(pictures: List<MultipartFile>): List<MealPictureDto> {
+    fun handleFileUpload(pictures: List<MultipartFile>, date: LocalDate?): List<MealPictureDto> {
         return try {
-            val dtos = pictures.map { it -> MealPictureDto(0, LocalDate.now(), it.originalFilename) }
+            //Check if date is not null
+            //Check if pictures is not empty
+            val dtos = pictures.map { it -> MealPictureDto(0, date, it.originalFilename) }
             //Save to model the DB
             //Save the pictures in a folder
             return dtos
