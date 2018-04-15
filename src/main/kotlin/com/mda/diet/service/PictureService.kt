dfb2da8d6233225f604de62e9cfb0c3cb194b524
MDA06 <michaelvm171@msn.com>
2018-04-15 10:36:29 +0200

    Save the uploaded image un the current user home directory

--- src/main/kotlin/com/mda/diet/service/PictureService.kt
+++ src/main/kotlin/com/mda/diet/service/PictureService.kt
@@ -9,11 +9,15 @@ import com.mda.diet.repository.MealPictureRepository
 import org.springframework.security.core.context.SecurityContextHolder
 import org.springframework.stereotype.Service
 import org.springframework.web.multipart.MultipartFile
+import java.io.File
+import java.nio.file.Files
 import java.time.LocalDate
 
 @Service
 class PictureService(val repository: MealPictureRepository,
                      val customerRepository: CustomerRepository) {
+
+    val rootLocation = System.getProperty("user.home")!! + "/diet-api/"
 
     fun handleFileUpload(pictures: List<MultipartFile>, date: LocalDate?): List<MealPictureDto> {
         try {
@@ -21,19 +25,39 @@ class PictureService(val repository: MealPictureRepository,
             if(pictures.isEmpty()) throw  UploadFileException("No pictures was given")
             //Check patient !
             val customer = customerRepository.getByAuthId(SecurityContextHolder.getContext().authentication.principal.toString())
-            if(customer == null || customer !is Patient)
-                throw UploadFileException("The current customer is not a patient !")
+                    as? Patient ?: throw UploadFileException("The current customer is not a patient !")
 
             val models = repository.save(pictures.map {
                 MealPicture(0, it.originalFilename, date, customer)
             })
 
-            //Save the pictures in a folder
-            //If a error occurs delete from the DB !
+            try {
+                val patientDir = customer.authId?.replace("|", "")!!
+                createDirectoriesIfNotExist(patientDir)
+                pictures.forEach {
+                    Files.copy(it.inputStream, File("$rootLocation/$patientDir/${it.originalFilename}").toPath())
+                }
+            } catch(ex: Exception)
+            {
+                //If a error occurs delete from the DB !
+                repository.delete(models)
+                throw UploadFileException("Failed to upload pictures: ${ex.javaClass.name} - ${ex.message}")
+            }
 
             return models.map { MealPictureDto(it) }
         } catch (e: Exception) {
-            throw UploadFileException("Failed to upload pictures ${e.message}")
+            throw UploadFileException("Failed to upload pictures: ${e.javaClass.name} - ${e.message}")
         }
+    }
+
+    fun createDirectoriesIfNotExist(subDir: String)
+    {
+        val rootDirectory = File(rootLocation)
+        if(!rootDirectory.exists())
+            rootDirectory.mkdir()
+        val uploadDir = rootLocation + subDir
+        val directory = File(uploadDir)
+        if(!directory.exists())
+            directory.mkdir()
     }
 }
