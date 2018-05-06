62996665bc9011d29c6a0bc6ba4e5abcf64c293b
MDA06 <michaelvm171@msn.com>
2018-05-06 14:05:47 +0200

    WIP: retrieve participants authorization

--- src/main/kotlin/com/mda/diet/controller/ChatController.kt
+++ src/main/kotlin/com/mda/diet/controller/ChatController.kt
@@ -1,9 +1,15 @@
 package com.mda.diet.controller
 
 import com.mda.diet.dto.ChatMessageDto
+import com.mda.diet.dto.DietetistDto
+import com.mda.diet.dto.RoleDto
 import com.mda.diet.error.CustomerNotFoundException
+import com.mda.diet.model.Admin
 import com.mda.diet.model.ChatParticipant
+import com.mda.diet.model.Dietetist
+import com.mda.diet.model.Patient
 import com.mda.diet.repository.ChatParticipantRepository
+import com.mda.diet.repository.CustomerRepository
 import org.springframework.messaging.handler.annotation.DestinationVariable
 import org.springframework.messaging.handler.annotation.MessageMapping
 import org.springframework.messaging.handler.annotation.Payload
@@ -17,12 +23,33 @@ import java.util.*
 
 @Controller
 class ChatController(val chatParticipantRepository: ChatParticipantRepository,
+                     val customerRepository: CustomerRepository,
                      val simpMessagingTemplate: SimpMessagingTemplate) {
 
     @SubscribeMapping("/chat.participants")
     fun retrieveParticipants(headerAccessor: SimpMessageHeaderAccessor): List<ChatParticipant> {
         val participant = chatParticipantRepository.findBySessionId(headerAccessor.sessionId)
-        return chatParticipantRepository.findAll().filter { it.authId != participant?.authId }.toList()
+        val customer = customerRepository.getByAuthId(participant?.authId ?: "")
+
+        val iterable = chatParticipantRepository.findAll()
+                .filter { it.authId != participant?.authId }
+
+        //TODO: Check the participants here but also in connect/disconnect...
+
+//        when(customer) {
+//            is Dietetist -> {
+//                //Diet can only see his patients
+//                iterable = iterable.filter { customer.patients.map { it.authId }.contains(it.authId) }
+//            }
+//
+//            is Patient -> {
+//                //Patient can only see his diet
+//                iterable = iterable.filter { it.authId == customerRepository.findOne(customer.dietetistId).authId}
+//            }
+//            //Admin can see all the participants
+//        }
+
+        return iterable.toList()
     }
 
     @MessageMapping("/send/msg")
